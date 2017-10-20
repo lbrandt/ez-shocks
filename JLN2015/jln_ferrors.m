076214ba9b75f3240e6872996fbc34513fa97430
lbrandt <lennart@rohde-brandt.de>
2017-10-20 19:22:45 +0200

    Started implementing forecasting regression using extracted factors.

--- JLN2015/jln_ferrors.m
+++ JLN2015/jln_ferrors.m
@@ -8,17 +8,19 @@ clear; clc;
 load jlndata; % includes all 132 macro series & 157 financial series 1960:01 .. 2011:12
 ind         = 132+(6:15); % column index of series to be removed, 6:15 in fin block    
 data(:,ind) = []; % delete series in data matrix
-names(ind)  = []; % delete sereis in name vector
+names(ind)  = []; % delete series in name vector
 xt          = data;
 
 % Estimate factors
 [e,fhat,lf,vf] = jln_factors(xt,20,2,2);
+[e,ghat,lg,vg] = jln_factors(xt.^2,20,2,2);
 
-
-
-[e,ghat,lg,vg] = factors(xt.^2,20,2,2);
+% Calculate marginal R2 (unused)
 outf     = 'mR2_fhat.out';
 outg     = 'mR2_ghat.out';
+
 [R2,mR2] = mrsq(fhat,lf,vf,names,vartype,outf);
 [R2,mR2] = mrsq(ghat,lg,vg,names,vartype,outg);
-ft       = [fhat,fhat(:,1).^2,ghat(:,1)]; %predictor set
+
+% Build predictor set
+ft       = [fhat,fhat(:,1).^2,ghat(:,1)];
--- MATLAB/factor_forecast.m
+++ MATLAB/factor_forecast.m
@@ -6,12 +6,14 @@
 %
 % -------------------------------------------------------------------------
 
+clear; clc;
+
 % Load and manipulate data in import_data. Call results here:
 import_data
 
 
 % Simulate data as in testnfac.m by Serena Ng
-clear; clc;
+%clear; clc;
 rng(999);
 
 r = 4; 
@@ -25,6 +27,7 @@ x = f*lambda'+ e;
 
 fprintf('Data generating process: r = %d \n', r);
 fprintf('Sample: T = %d, N = %d \n', size(x));
+
 
 %%%%
 % Find optimal number of factors according to Bai & Ng (2002)
@@ -55,6 +58,36 @@ sumeigval = cumsum(evf)/sum(evf);
 R2_static = sum(evf(1:rhat))/sum(evf);
 
 
+%%%%
+% Forecast
+
+% Build predictor set
+zt     = [Fhat, Fhat(:,1).^2, Ghat(:,1)];
+
+% Set dependent variables
+yt     = standardise(x(:,1:132)); % only macro data
+[T, N] = size(yt);
+
+py     = 4; % number of depvar lags
+pz     = 2; % number of predictor lags
+maxlag = max(py,pz);
+
+q      = fix(4*(T/100)^(2/9)); % Newey-West lag length according to rule-of-thumb (N&W 1994)
+
+
+ybetas = zeros(1+py+pz*size(zt,2),N);
+
+for i = 1:N
+    X    = [ones(T,1),mlags(yt(:,i),py),mlags(ft,pz)];
+    reg  = nwest(yt(p+1:end,i),X(p+1:end,:),q);
+    pass = abs(reg.tstat(py+2:end)) > 2.575; % hard threshold
+    keep = [ones(1,py+1)==1,pass'];
+    Xnew = X(:,keep);
+    reg  = nwest(yt(p+1:end,i),Xnew(p+1:end,:),q);
+    vyt(:,i)       = reg.resid; % forecast errors
+    ybetas(keep,i) = reg.beta;   
+    fmodels(:,i)   = pass; %chosen predictors
+end
 
 
 
