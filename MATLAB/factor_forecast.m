483631bcb9ba64a5e5526133d3ebe9693db95f8f
lbrandt <lennart@rohde-brandt.de>
2017-10-20 18:23:40 +0200

    Renamed generate_factors.m to factor_forecast.m
    Do model estimation and forecasting in same script.

--- MATLAB/factor_forecast.m
+++ MATLAB/factor_forecast.m
@@ -1,21 +1,14 @@
 % -------------------------------------------------------------------------
-% Generate factors
+% Generate forecast via factor model
 %
 %
 %
 %
 % -------------------------------------------------------------------------
 
-%%%%
-% Import data
+% Load and manipulate data in import_data. Call results here:
+import_data
 
-% JLN 2015
-clear; clc;
-load jlndata; 
-ind         = 132+(6:15); % "duplicate" series to remove    
-data(:,ind) = []; 
-names(ind)  = [];
-x           = data;
 
 % Simulate data as in testnfac.m by Serena Ng
 clear; clc;
@@ -33,9 +26,9 @@ x = f*lambda'+ e;
 fprintf('Data generating process: r = %d \n', r);
 fprintf('Sample: T = %d, N = %d \n', size(x));
 
+%%%%
 % Find optimal number of factors according to Bai & Ng (2002)
-
-kmax = 10; % Max number of factors to be extracted
+kmax = 20; % Max number of factors to be extracted
 gnum = 2; % ICp2 chosen in JLN2015
 demean = 2; % Standardise data
 
@@ -46,15 +39,22 @@ end
 
 bnicmin = min(bnicv);
 rhat = minind(bnicv); % Optimal number of factors according to lowest IC
-fprintf('\nFactors via IC: rhat = %d \n', rhat);
+fprintf('\nFactors via IC(%d): rhat = %d \n', gnum, rhat);
 
+
+%%%%
 % Extract factors via PCA
-[ehat1,Fhat1,lamhat1,ev1]  = jln_factors(x,kmax,gnum,demean);
-[Fhat, Lhat, ehat, ev] = factors(x, rhat, demean);
 
-sumeigval = cumsum(ev)/sum(ev);
-R2_static = sum(ev(1:rhat))/sum(ev);
+%[ehat1, Fhat1, lamhat1, ev1]  = jln_factors(x, kmax, gnum, demean);
+%[ehat1, Ghat1, lamhat1, ev1]  = jln_factors(x.^2, kmax, gnum, demean);
 
-% Save factors for further analysis
+[Fhat, LF, ef, evf] = factors(x, rhat, demean);
+[Ghat, LG, eg, evg] = factors(x.^2, rhat, demean);
+
+sumeigval = cumsum(evf)/sum(evf);
+R2_static = sum(evf(1:rhat))/sum(evf);
+
+
+
 
 
