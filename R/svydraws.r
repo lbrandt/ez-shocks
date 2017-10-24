0757444d1e2288b907fe0a7b71d003a821d460ab
lbrandt <lennart@rohde-brandt.de>
2017-10-24 16:24:06 +0200

    Added R Code for volatility model

---
+++ R/svydraws.r
@@ -0,0 +1,34 @@
+# -------------------------------------------------------------------------
+# Estimate a first-order autoregressive stochastic volatility model on the
+# forecast errors from the macroeconomic data (no conditional mean)
+# -------------------------------------------------------------------------
+
+# Initialization
+rm(list=ls())
+library(stochvol)
+set.seed(0) # for replication
+options(digits=17)
+vt   = read.table('vyt2014.txt',sep = '\t')
+T    = dim(vt)[1]
+N    = dim(vt)[2]
+for (i in 1:N){
+	if(min(log(vt[,i]^2))== -Inf){
+		vt[,i] = vt[,i] + 0.00001 #offset to avoid taking log of zero
+	}
+}
+
+# Run MCMC algorithm and store draws
+S    = 50000
+burn = 50000
+m    = matrix(0,T+3,N)
+g    = matrix(0,3,N)
+for (i in 1:N){
+	draws  = svsample(vt[,i],draws=S,burnin=burn,quiet=TRUE,thinpara=10,thinlatent=10)
+	all    = cbind(draws$para,draws$latent)
+	m[,i]  = colMeans(all)
+	g[,i]  = geweke.diag(draws$para)$z
+	name   = sprintf('svydraws%d.txt',i)
+#	write(t(all),file=name,ncolumn=dim(all)[2])
+}
+out = rbind(m,g) #include Geweke statistics
+write(t(out),file='svymeans2014.txt',ncolumn=dim(out)[2])
