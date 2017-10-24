8c1f11ebe8764c34c56c6b93db2c394e29b8c396
lbrandt <lennart@rohde-brandt.de>
2017-10-24 17:48:34 +0200

    Format variable names for .csv table
    varnames now contains permissible MATLAB varnames

--- MATLAB/import_data.m
+++ MATLAB/import_data.m
@@ -1,5 +1,5 @@
 % -------------------------------------------------------------------------
-% Import data
+% Import JLN data
 %
 % 
 %
@@ -20,5 +20,24 @@ ta = datetime(1960, 03, 15);
 te = datetime(2011, 12, 15);
 dates = (ta:calmonths(1):te)';
 
+% Format names such that they are permissible MATLAB varnames
+varnames = strrep(names, ' ', '_');
+varnames = strcat('var_', varnames);
+varnames = strrep(varnames, '&', '_');
+varnames = strrep(varnames, '/', '_');
+varnames = strrep(varnames, '\', '_');
+varnames = strrep(varnames, ':', '');
+varnames = strrep(varnames, '.', '');
+varnames = strrep(varnames, ',', '');
+varnames = strrep(varnames, '+', '');
+varnames = strrep(varnames, '-', '');
+varnames = strrep(varnames, '<', '');
+varnames = strrep(varnames, '>', '');
+varnames = strrep(varnames, '(', '');
+varnames = strrep(varnames, ')', '');
+varnames = strrep(varnames, '’', '');
+
+
 fprintf('Monthly series from %s to %s \n', datestr(ta), datestr(te));
 fprintf('Sample: T = %d, N = %d \n', T, N);
+
