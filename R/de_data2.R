e12f9a4f466f5bb31f850ec71a8309469985ab4c
lbrandt <lennart@rohde-brandt.de>
2017-11-16 18:02:32 +0100

    Split data check into another function.

--- R/de_data2.R
+++ R/de_data2.R
@@ -30,7 +30,8 @@ data = datastream %>%
   select(-starts_with("X__")) %>% # Delete separator columns
   select(-starts_with("Code")) %>% # Delete date columns
   
-  rename(WGUN_TOTQ = "WGUN%TOTQ") %>% # Replace R operator % in variable names
+  # Replace R operator % in variable names
+  rename(WGUN_TOTQ = "WGUN%TOTQ") %>%
   
   # Aggregate value added
   mutate(bwfinanzverm = (BDVAPAICD*BDVAPAICB + BDVAPAFID*BDVAPAFIB + BDVAPARED*BDVAPAREB + BDVAPASTD*BDVAPASTB)/
@@ -44,6 +45,7 @@ data = datastream %>%
 
 
 # Extending time series via growth rates of related variables
+source("fun.chain.R")
 
 # Chain link UNP with UNP in Western Germany
 data$unrate = fun.chain(data$BDUSCC02Q, data$WGUN_TOTQ, 0, 0)
@@ -53,15 +55,18 @@ data$eonia = fun.chain(data$BDSU0304R, data$BDSU0101, 0, 0)
 data$is1mo = fun.chain(data$BDSU0310R, data$BDSU0104, 0, 0)
 data$is3mo = fun.chain(data$BDSU0316R, data$BDSU0107, 0, 0)
 
-
-
-
+# Chain link Turnover in construction
+data$toconstind = fun.chain(data$BDUSMC31B, data$BDUSMB31B, 0, 0)
+data$toconstres = fun.chain(data$BDUSMC36B, data$BDUSMB36B, 0, 0)
+data$toconstpub = fun.chain(data$BDUSMC37B, data$BDUSMB37B, 0, 0)
 
 
 
 
 # Extract survey indicators
 surveys = select(data, starts_with("BDIFD"))
+
+
 
 
 # Remove unnecessary or discontinued series from data
@@ -71,39 +76,29 @@ data = data %>%
   select(-c(BDVAPAAHD, BDVAPAOSD, BDVAPAAHB, BDVAPAOSB)) %>% # bwoeffprivdl
   select(-c(BDGCMAC.B, BDGCMAC.D)) %>% # piar
   
-  select(-c(BDEA4001B, BDEA4100B, BDEA4170B, BDEA4220B)) %>% # Current account
+  select(-c(BDIPMAN.G__1)) %>% # Duplicate series
+  select(-c(BDGOVBALA, BDBQ9059A, BDGGOVBLA)) %>% # Government balance
   
   select(-c(BDUSCC02Q, WGUN_TOTQ)) %>% # Unemployment rate
   select(-c(BDSU0304R, BDSU0310R, BDSU0316R)) %>% # New short term interest rates
   select(-c(BDSU0101, BDSU0104, BDSU0107)) %>% # Old short term interest rates
+  select(-c(BDUSMC36B, BDUSMC31B, BDUSMC37B)) %>% # New turnover in construction
+  select(-c(BDUSMB36B, BDUSMB31B, BDUSMB37B)) %>% # Old turnover in construction
   
   select(-c(BDUSC.04O, BDUSCC04O)) %>% # Vacancies are in series BDQLM004O
+  select(-c(BDEA4001B, BDEA4100B, BDEA4170B, BDEA4220B)) %>% # Current account
 
-  
   select(-starts_with("BDIFD")) # Survey indicators
 
 
 
-de.T = dim(data)[1]
-de.N = dim(data)[2] 
 
+# Save data to workspace and .csv
+save(dates, data, surveys, file = "de_data2.RData")
 
-# Check against PW
+out.data    = add_column(data, dates = dates, .before = 1)
+out.surveys = add_column(surveys, dates = dates, .before = 1)
 
-
-
-data$is1mo = fun.chain(data$BDSU0310R, data$BDSU0104, 0, 0)
-
-eonia
-is1mo
-
-plot(0, type="n", xlab="Time", ylab="Series", xlim = c(1,de.T), ylim = c(0, 15))
-
-
-lines(data$BDSU0310R, col = "blue")
-lines(data$BDSU0104)
-lines(data$is1mo)
-lines(pw_data_final$X__45)
-
-
+write.csv(out.data, file = 'de_data2.csv', row.names = FALSE)
+write.csv(out.surveys, file = 'de_surveys.csv', row.names = FALSE)
 
---
+++ R/de_data_check.R
@@ -0,0 +1,26 @@
+# Check de_data2
+
+#rm(list=ls())
+load("de_data2.RData")
+
+
+de.T = dim(data)[1]
+de.N = dim(data)[2] 
+
+
+
+
+plot(0, type="n", xlab="Time", ylab="Series", xlim = c(1, de.T), ylim = c(0, 100))
+
+plot(data$BDBQ9059A)
+
+lines(data$BDDB_GDP , col = "blue")
+lines(data$BDBQ9059A, col = "red")
+
+lines(diff(log(data$BDUSMB36B)))
+lines(diff(log(data$BDUSMC36B)))
+
+lines(pw_data_final$X__45)
+
+
+test3 = diff(log(data$BDUSMB36B))/diff(log(data$BDUSMC36B))
