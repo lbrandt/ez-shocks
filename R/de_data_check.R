0d2bbbd34e41a7e5984304c94b0f9f338d01a8b4
lbrandt <lennart@rohde-brandt.de>
2017-11-17 19:09:10 +0100

    Implemented output plot for eyeballing stationarity and seasonality

--- R/de_data_check.R
+++ R/de_data_check.R
@@ -2,12 +2,38 @@
 
 #rm(list=ls())
 
+require(tidyverse)
 
 load("de_data2.RData")
 
 
 de.T = dim(data)[1]
 de.N = dim(data)[2] 
+
+qplot(1:de.T, data$BDPOPTOTP) + geom_line()
+
+
+# From stats pc tut
+path = file.path(getwd(), paste("de_data_plot.pdf"))
+pdf(file = path)
+
+par(mfrow = c(3,2))
+
+for(i in 1:de.N){
+
+  plot(data[[i]], type = "l", col = "blue", xlab = "Time", ylab = "Variable",
+       main = colnames(data[i]), lwd = 2)
+  #lines(n, Y2, col = "green", lwd = 2)
+  #legend("topright", legend = c("T_n", "T'_n"), fill = c("blue", "green"))
+  #box()
+  
+}
+dev.off()
+par(mfrow = c(1,1))
+
+
+
+
 
 
 
@@ -19,10 +45,3 @@ plot(data$BDBQ9059A)
 lines(data$BDDB_GDP , col = "blue")
 lines(data$BDBQ9059A, col = "red")
 
-lines(diff(log(data$BDUSMB36B)))
-lines(diff(log(data$BDUSMC36B)))
-
-lines(pw_data_final$X__45)
-
-
-test3 = diff(log(data$BDUSMB36B))/diff(log(data$BDUSMC36B))
