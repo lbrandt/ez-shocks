f83507c47a2d8542b4ae11dffdd0485ff409aab1
Lennart Brandt <lennart@rohde-brandt.de>
2017-12-27 14:23:14 +0100

    Implemented check for consistency of factor estimates when data contains duplicate series.

--- MATLAB/sim_fac.m
+++ MATLAB/sim_fac.m
@@ -2,6 +2,7 @@
 % Static factor model
 
 %clear;clc;
+rng(58);
 
 % Simulate static factor model with R latent factors and M observed variables
 N = 100;
@@ -87,8 +88,6 @@ fprintf('min(xrho) = %d, max(xrho) = %d \n', min(min(xrho)), max(max(xrho - eye(
 %%%% corrplot requires Econometrics Toolbox!
 %corrplot(X)
 
-
-
 %%%%
 % PCA
 demean = 2; % Standardise variables
@@ -125,7 +124,62 @@ for i = 1:R
     end
 end
 
-% Did PCA recover some rotation of the underlying factor space?
-[frmse1, fbias, fvar] = rmse(F(:, 1), Fhat(:, 1));
+% RMSE(F_1, Fhat_1) = 0.3277
+% RMSE(F_2, Fhat_2) = 0.7149
 
 
+%%%%
+% Duplicate series
+% Analyse factor loadings
+Lhat
+
+% Here, first observable X_1 is highly correlated with second factor. Thus,
+% saturate X matrix with X_1 and check stability of factor estimators. When
+% X_1 is simply duplicated (rank(X) = M) and when Normal disturbance is
+% added (rank(X2) = M + duplicate), identification eventually switches. 
+% The first factor is then represented by the second extracted component 
+% and the second factor is represented by the first extracted component. 
+% This is likely due to its variance eventually dominating the eigenvalues 
+% of the covariance matrix. This can lead to problems when too few
+% principal components are extracted. Then the true structural factor space
+% will not be recovered but instead only that part which is dominating the 
+% sample variance. It might therefore be sensible to choose a more lenient
+% information criterion for estimating R.
+
+X2 = X;
+
+duplicate = 20;
+
+fcorrmat2 = zeros(R, Rhat, duplicate);
+frmse2    = zeros(R, Rhat, duplicate);
+
+for s = 1:duplicate
+    
+    X2 = [X2, X2(:, 1) + randn(N, 1)];
+    
+    [Fhat2, Lhat2, Ehat2, eigval2] = factors(X2, Rhat, demean);
+    
+    for i = 1:R
+        for j = 1:Rhat
+            corrcoeffs2 = corrcoef(F(:, i), Fhat2(:, j));
+            fcorrmat2(i, j, s) = corrcoeffs2(2, 1); % Extract correlation
+            
+            frmse2(i, j, s)    = rmse(standardise(F(:, i)), standardise(Fhat2(:, j)));
+        end
+    end
+    
+end
+    
+figure
+subplotindex = 1;
+for i = 1:R
+    for j = 1:Rhat
+        subplot(R, Rhat, subplotindex)
+        
+        plot(squeeze(abs(fcorrmat2(i, j, :))))
+        %plot(squeeze(frmse2(i, j, :)))
+        title(['F(', num2str(i), '), Fhat(', num2str(j), ')'])
+        
+        subplotindex = subplotindex + 1;
+    end
+end
