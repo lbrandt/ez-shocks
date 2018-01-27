52c39c19c419b6bcfd5bb1f86f0d479a2f9918e7
lbrandt <lennart@rohde-brandt.de>
2018-01-27 22:16:03 +0100

    Read quarterly shocks database by Georgiadis and Jancokova.

---
+++ R/gj_shocks.R
@@ -0,0 +1,22 @@
+# Georgiadis Jancokova Monetary Policy Shocks Database
+
+#rm(list=ls())
+
+require(tidyverse)
+require(readxl)
+require(h5)
+
+# Set working directory to script file location
+
+# Retrieve script location when code is run within RStudio
+location.thisfile = dirname(rstudioapi::getActiveDocumentContext()$path)
+# Retrieve script location if file is called via source()
+location.thisfile = dirname(sys.frame(1)$ofile)
+# Setting working directory to file location
+setwd(location.thisfile)
+
+# Set location of data relative to working directory which contains this script
+location.data = normalizePath(file.path("..", "..", "..", "Data"), winslash = "/")
+
+gj_import_q = read_dta(file.path(location.data, "Georgiadis Jancokova Shocks Database", "mpshocks_database_public.dta"))
+
