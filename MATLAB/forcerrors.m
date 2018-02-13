e79d45df64a4b25811665f8cfd82fd0daf0af7e6
Lennart <lennart@rohde-brandt.de>
2018-02-13 19:25:27 +0100

    Nonfunctional code for forecast experiment in MATLAB.

--- MATLAB/forcerrors.m
+++ MATLAB/forcerrors.m
@@ -1,5 +1,4 @@
 % Forecast Evaluation
-% 
 
 regstart = 2;
 
@@ -42,3 +41,50 @@ for i = 1:femax
     fl(i) = ff(i) - norm_inv(0.5 + 0.5*coverage)*rmse;
     
 end
+
+
+%%%%%%
+
+hdip = 5;
+pmax = max(pfac, pdip);
+
+diplags = mlag(dip, pdip);
+diplead = lead(dip, hdip);
+
+faclags = mlag(Fhat, pfac);
+dipvars = [ones(T, 1), diplags, faclags];
+
+holdout = 10;
+
+% Full sample
+fullsample = 1+pmax:T-hdip;
+
+h1model = ols(dip(fullsample), dipvars(fullsample, :));
+h2model = ols(diplead(fullsample, 1), dipvars(fullsample, :));
+h3model = ols(diplead(fullsample, 2), dipvars(fullsample, :));
+
+
+forcerrors = zeros(holdout, 1);
+for i = 1:holdout
+    
+    i = 10
+    
+    fsample = pmax+i-1:(T-holdout-hdip+i); % Rolling window
+    h1model = ols(dip(fsample, :), dipvars(fsample, :));
+    h2model = ols(diplead(fsample, 1), dipvars(fsample, :));
+    
+    forcmodel.yhat(end)
+    
+    matchsample(dates, dates(T-holdout))
+    
+    ff1 = dipvars(T-holdout+i, :)*forcmodel.beta;
+    ff2 = mlag(dipvars(fsample+1, :), 1)
+
+    forecast
+end
+dipmodel = ols(dip, dipvars);
+prt(dipmodel);
+
+plot(dip)
+hold on
+plot(dipmodel.yhat)
